25c1ad65011bb18d00dfd1fc178b09bea1d71014
krasotun <servrostov@gmail.com>
2026-01-24 10:17:17 +0300

    add: sign-in form;

--- src/components/sign-in-form/SignInForm.ts
+++ src/components/sign-in-form/SignInForm.ts
@@ -17,7 +17,48 @@ export type SignInFormProps = BlockProps & {
 };
 
 export class SignInForm extends Block<SignInFormProps> {
+  constructor(props: SignInFormProps) {
+    super({
+      ...props,
+      events: {
+        ...(props.events ?? {}),
+      },
+    });
+
+    this._setHandlers();
+  }
+
   render(): DocumentFragment {
     return this.renderTemplate(template);
   }
+
+  private _setHandlers(): void {
+    this.setProps({
+      events: {
+        ...(this.props.events ?? {}),
+        submit: this._handleSubmit,
+      },
+    });
+  }
+
+  private _handleSubmit = (event: Event) => {
+    event.preventDefault();
+    const { loginInput, passwordInput } = this.children as SignInFormProps['children'];
+    const isLoginValid = loginInput.validate();
+    const isPasswordValid = passwordInput.validate();
+
+    if (!isLoginValid || !isPasswordValid) {
+      console.log('Данные не валидны');
+      return;
+    }
+
+    const values = {
+      [loginInput.name]: loginInput.value,
+      [passwordInput.name]: passwordInput.value,
+    };
+
+    console.log({
+      ...values,
+    });
+  };
 }
